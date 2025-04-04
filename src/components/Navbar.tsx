44b0cf273e3a1aedd416ad9121874fc4f2c8126d
Malu Romani <romani.malu84@gmail.com>
2025-04-04 12:40:42 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,8 +49,8 @@ const Navbar = () => {
                 </div>
               </Link>
               <Link 
-                to="/add-book" 
-                className={`px-3 py-2 rounded-md hover:bg-secondary transition-colors ${isActive('/add-book')}`}
+                to="/AddBook" 
+                className={`px-3 py-2 rounded-md hover:bg-secondary transition-colors ${isActive('/AddBook')}`}
               >
                 <div className="flex items-center gap-2">
                   <PlusCircle size={18} />
